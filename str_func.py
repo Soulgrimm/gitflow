369c085dddb242d70eba822b6d7326026dba62c7
Pavel gromov <Dante94_94@mail.ru>
2023-08-06 17:22:10 +0300

    add ew function and descriptions

--- str_func.py
+++ str_func.py
@@ -1,2 +1,16 @@
 def change_cap_text(text):
+    """
+    Делает все буквы заглавными.
+    :param text:
+    :return:
+    """
     return print(text.upper())
+
+
+def upper_first_letters(text):
+    """
+    Делает первые буквы заглавными.
+    :param text:
+    :return:
+    """
+    return text.title()
